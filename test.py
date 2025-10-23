872beabd25d5fad1ee0ac1af6a7e77b9fd51727c
Owy137 <yango@etown.edu>
2025-10-23 12:00:15 -0400

    removed test

--- test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import subprocess
-import os
-from rapidocr import RapidOCR
-
-stringCheck = ("newbalance", "closingbalance")
-
-if("never" in stringCheck):
-    print("matches string check")
