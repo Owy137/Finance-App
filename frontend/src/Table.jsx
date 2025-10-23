c2ccaa459fd10a75307cb1d1d1f2a9fe286eb0a2
Owy137 <yango@etown.edu>
2025-10-23 11:58:49 -0400

    changes

--- frontend/src/Table.jsx
+++ frontend/src/Table.jsx
@@ -13,6 +13,22 @@ function FileTable() {
                     <th>Custom Amount</th>
                 </tr>
             </thead>
+            <tbody>
+                <tr>
+                    <td><button type="checkbox" id="includeBox"></button></td>
+                    <td>Test File 1</td>
+                    <td>1234</td>
+                    <td>34</td>
+                    <td><input type="number" placeholder="Enter amount"/></td>
+                </tr>
+                <tr>
+                    <td><button type="checkbox" id="includeBox"></button></td>
+                    <td>Test File 2</td>
+                    <td>2345</td>
+                    <td>45</td>
+                    <td><input type="number" placeholder="Enter amount"/></td>
+                </tr>
+            </tbody>
         </table>
     );
 }
